b7e218a6a5700052f2540021409a42e4f978c763
Vitalii <0x1d46@gmail.com>
2022-03-26 00:18:31 +0100

    add position count

--- src/assets/initialSession.ts
+++ src/assets/initialSession.ts
@@ -3,5 +3,6 @@ import { SessionData } from '../types';
 export const initialSession = (): SessionData => ({
   route: '',
   lastMessageTable: undefined,
-  timezone: null
+  timezone: null,
+  countPositions: 0
 });
--- src/middlewares/setPrintTable.ts
+++ src/middlewares/setPrintTable.ts
@@ -7,29 +7,32 @@ export const setPrintTable = (ctx: CustomContext, next: NextFunction): Promise<v
   ctx.printOrEditTable = async function(records, message_id) {
     const { timezone } = ctx.session;
     const t = new Table;
+    let count = 0;
 
     for (const record of records) {
       const { time } = ISOtoLocalDate(record.date, timezone ?? 0);
+      t.cell('№', ++count, Table.number());
       t.cell('Time', time);
       t.cell('Position', record.position, positionPrinter);
       t.cell('Price', +record.price.toFixed(2), Table.number());
       t.newRow();
     }
+    ctx.session.countPositions = count;
 
     t.total('Price');
 
     if (message_id) { // @ts-ignore
       return await ctx.api.editMessageText(ctx.user.id, message_id, `<pre>${t}</pre>`, { parse_mode: 'HTML' });
-    } else
+    } else {
       return await this.reply(`<pre>${t}</pre>`, { parse_mode: 'HTML' });
+    }
   };
   return next();
 };
 
 function positionPrinter(val: string | undefined, width: number): string {
   let result = '';
-  if (!val)
-    while (width--)
-      result += '.';
+  while (!val && width--)
+    result += '.';
   return val ?? result;
 }
