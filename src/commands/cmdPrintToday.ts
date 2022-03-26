50c1def259cb30a68320fb8e294662086f1c9ba8
Vitalii <0x1d46@gmail.com>
2022-03-26 18:13:30 +0100

    -

--- src/commands/cmdPrintToday.ts
+++ src/commands/cmdPrintToday.ts
@@ -5,7 +5,7 @@ export const cmdPrintToday = async (ctx: CustomContext) => {
   const date = ctx.state.today;
   const { id } = ctx.user;
   const records = await User.getRecords(id, date);
-  
+
   if (!records || !records.length) return ctx.reply('Сегодня покупок нету');
   const { message_id } = await ctx.printOrEditTable(records);
   ctx.session.lastMessageTable = message_id;
