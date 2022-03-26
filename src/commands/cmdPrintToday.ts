281980cc7647228907f851852a01d486e4a5d72c
Vitalii <0x1d46@gmail.com>
2022-03-26 17:21:16 +0100

    add today shopping table

---
+++ src/commands/cmdPrintToday.ts
@@ -0,0 +1,12 @@
+import { CustomContext } from '../types';
+import { User } from '../mongoose';
+
+export const cmdPrintToday = async (ctx: CustomContext) => {
+  const date = ctx.state.today;
+  const { id } = ctx.user;
+  const records = await User.getRecords(id, date);
+  
+  if (!records || !records.length) return ctx.reply('Сегодня покупок нету');
+  const { message_id } = await ctx.printOrEditTable(records);
+  ctx.session.lastMessageTable = message_id;
+};
