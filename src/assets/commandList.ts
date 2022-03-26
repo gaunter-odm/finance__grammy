aa8ce5d6d29e84565417705a23f13a270c002395
Vitalii <0x1d46@gmail.com>
2022-03-26 17:21:58 +0100

    add /today command

--- src/assets/commandList.ts
+++ src/assets/commandList.ts
@@ -2,4 +2,5 @@ export const commandList = [
   { command: 'start', description: 'Start the bot' },
   { command: 'help', description: 'Show help text' },
   { command: 'settings', description: 'Open settings' },
+  { command: 'today', description: 'Покупки за сегодня' },
 ]
