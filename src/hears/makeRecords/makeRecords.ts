757bf76197ab48bcb07b8051b07ef067a60b2956
Vitalii <0x1d46@gmail.com>
2022-03-27 16:00:41 +0200

    save last date

--- src/hears/makeRecords/makeRecords.ts
+++ src/hears/makeRecords/makeRecords.ts
@@ -1,6 +1,6 @@
-import { CustomContext } from '../types';
-import { rawStringToRecords } from '../helpers/rawStringToRecords';
-import { User } from '../mongoose';
+import { CustomContext } from '../../types';
+import { rawStringToRecords } from '../../helpers/rawStringToRecords';
+import { User } from '../../mongoose';
 
 export const makeRecords = async (ctx: CustomContext) => {
   const last = ctx.session.lastMessageTable;
@@ -10,4 +10,5 @@ export const makeRecords = async (ctx: CustomContext) => {
   const { message_id } = await ctx.printOrEditTable(records, last);
   setTimeout(() => ctx.deleteMessage(), 500);
   if (!last && message_id) ctx.session.lastMessageTable = message_id;
+  ctx.session.lastPurchaseDate = ctx.state.today
 };
