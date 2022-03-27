e119fe663d39c972193dc5d585c6610b1cded32d
Vitalii <0x1d46@gmail.com>
2022-03-27 15:46:58 +0200

    add hide prev day

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Bot, session } from 'grammy';
+import { Bot, InlineKeyboard, session } from 'grammy';
 import { token } from './src/config';
 import { commandList } from './src/assets/commandList';
 import { name_and_price } from './src/regexp';
@@ -7,12 +7,13 @@ import { CustomContext } from './src/types';
 import { setState } from './src/middlewares/setState';
 import { setUserData } from './src/middlewares/setUserData';
 import { router } from './src/routes/router';
-import { makeRecords } from './src/hears/makeRecords';
 import { cmdStart } from './src/commands/cmdStart';
 import { initialSession as initial } from './src/assets/initialSession';
 import { setPrintTable } from './src/middlewares/setPrintTable';
 import { setTimeZone } from './src/middlewares/setTimeZone';
 import { cmdPrintToday } from './src/commands/cmdPrintToday';
+import { makeRecords } from './src/hears/makeRecords/makeRecords';
+import { hidePrevDay } from './src/hears/makeRecords/hidePrevDay';
 
 const bot = new Bot<CustomContext>(token);
 
@@ -29,10 +30,8 @@ bot.use(router);
 bot.command('start', cmdStart);
 bot.command('today', cmdPrintToday);
 
-bot.hears(name_and_price, makeRecords);
+bot.hears(name_and_price, hidePrevDay, makeRecords);
 
-bot.hears(/./, ctx => ctx.deleteMessage());
-bot.on('message:sticker', ctx => ctx.deleteMessage());
-bot.on('message:document', ctx => ctx.deleteMessage());
+bot.on('message', ctx => ctx.deleteMessage());
 
 bot.start().catch(console.log);
