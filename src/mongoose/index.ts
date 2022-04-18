ebdc07b40dcd320d79c5e027064dc7cab566dbf3
Vitalii <0x1d46@gmail.com>
2022-04-18 18:23:23 +0200

    make MagikLink model

--- src/mongoose/index.ts
+++ src/mongoose/index.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import { mongoUrl } from '../config';
 import { userSchema } from './schemas/UserSchema';
-import { User as UModel, UserStatic } from '../types';
+import { Magik, User as UModel, UserStatic } from '../types';
+import { magikLinkSchema } from './schemas/MagikLinkSchema';
 
 export const connect = mongoose.createConnection(mongoUrl);
 export const User = connect.model<UModel, UserStatic>('User', userSchema);
+export const MagikLink = connect.model<Magik>('MagikLink', magikLinkSchema);
