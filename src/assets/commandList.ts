8c913653ebd39cafb9e0556dd230d4019841e961
Vitalii <0x1d46@gmail.com>
2022-04-18 18:30:16 +0200

    remove commands

--- src/assets/commandList.ts
+++ src/assets/commandList.ts
@@ -1,7 +1,7 @@
 export const commandList = [
-  { command: 'start', description: 'Start the bot' },
-  { command: 'help', description: 'Show help text' },
-  { command: 'settings', description: 'Open settings' },
+  // { command: 'start', description: 'Start the bot' },
+  // { command: 'help', description: 'Show help text' },
+  // { command: 'settings', description: 'Open settings' },
   { command: 'today', description: 'Покупки за сегодня' },
   { command: 'auth', description: 'Перейти на сайт' },
 ]
