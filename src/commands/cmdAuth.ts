35eff6e68b751a7355296029a2df515ce7147618
Vitalii <0x1d46@gmail.com>
2022-04-18 18:19:05 +0200

    make cmdAuth

---
+++ src/commands/cmdAuth.ts
@@ -0,0 +1,36 @@
+import { CustomContext } from '../types';
+import { MagikLink, User } from '../mongoose';
+import { InlineKeyboard } from 'grammy';
+
+export const cmdAuth = async (ctx: CustomContext) => {
+  const { id } = ctx.user;
+
+  const getUser = await User.findOne({ id });
+  if (!getUser) return;
+
+  const user = getUser._id;
+  const magikLink = new MagikLink({ user });
+
+  await magikLink.save();
+
+  const { magik } = magikLink;
+
+  const { message_id } = await ctx.reply('Для авторизации на сайте перейдите по ссылке:', {
+    reply_markup: new InlineKeyboard()
+      .url('Авторизироваться', `www.google.com/${magik}`),
+  });
+
+  new Promise((resolve, reject) => {
+    setTimeout(async function() {
+      try {
+        await ctx.deleteMessage();
+        await ctx.api.deleteMessage(id, message_id);
+      } catch (e) {
+        reject(e);
+      }
+    }, 1000);
+  }).catch(e => {
+    console.log('Error deleting message, file: src/commands/cmdAuth.ts', new Date());
+  });
+
+};
