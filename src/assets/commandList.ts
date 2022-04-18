65127028f281da581d0cf21ad089cf2b5de07a2b
Vitalii <0x1d46@gmail.com>
2022-04-18 18:19:45 +0200

    add auth command to commandList.ts

--- src/assets/commandList.ts
+++ src/assets/commandList.ts
@@ -3,4 +3,5 @@ export const commandList = [
   { command: 'help', description: 'Show help text' },
   { command: 'settings', description: 'Open settings' },
   { command: 'today', description: 'Покупки за сегодня' },
+  { command: 'auth', description: 'Перейти на сайт' },
 ]
